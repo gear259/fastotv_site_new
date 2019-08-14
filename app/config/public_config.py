bc6854c00673c64631583b07941bae4857bf1af9
topilski <atopilski@gmail.com>
2019-08-14 16:50:59 +0300

    Typo

--- app/config/public_config.py
+++ app/config/public_config.py
@@ -5,7 +5,7 @@ PUBLIC_CONFIG = {'site': {'title': 'FastoTV', 'keywords': 'video,cloud,iptv,play
                  'support': {'contact_email': 'support@fastogt.com',
                              'contact_address': 'Republic of Belarus, Minsk',
                              'community_channel': 'https://discord.gg/cnUXsws',
-                             'partners':[{'company': 'Tathya Technologies Pvt Ltd (OPC)', 'website': 'https://tathyatechnologies.com', 'country':'India']},
+                             'partners':[{'company': 'Tathya Technologies Pvt Ltd (OPC)', 'website': 'https://tathyatechnologies.com', 'country':'India'}]},
                  'project': {'version': '1.1.0', 'version_type': 'release',
                              'github': {'issues': 'https://github.com/fastogt/fastotv/issues',
                                         'repo': 'https://github.com/fastogt/fastotv'}
